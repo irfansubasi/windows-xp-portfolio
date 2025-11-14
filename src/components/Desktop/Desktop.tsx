b3aa1b70ad64bfd8ba7a16102ffd602c369b78c2
irfansubasi <136987051+irfansubasi@users.noreply.github.com>
2025-11-14 22:23:00 +0300

    change desktop for icon feature

--- src/components/Desktop/Desktop.tsx
+++ src/components/Desktop/Desktop.tsx
@@ -1,5 +1,30 @@
 import styles from './Desktop.module.css';
 
+interface DesktopIcon {
+  id: string;
+  name: string;
+  icon: string;
+}
+
+const desktopIcons: DesktopIcon[] = [
+  { id: 'resume', name: 'Resume', icon: '/assets/PDF.ico' },
+];
+
 export const Desktop = () => {
-  return <div className={styles.desktop}></div>;
+  return (
+    <div className={styles.desktop}>
+      <div className={styles['icons-container']}>
+        {desktopIcons.map((icon) => (
+          <div key={icon.id} className={styles.icon}>
+            <img
+              src={icon.icon}
+              alt={icon.name}
+              className={styles['icon-image']}
+            />
+            <span className={styles['icon-name']}>{icon.name}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
