811085a510d1717cc34dbc38b8a10796a8e87025
irfansubasi <136987051+irfansubasi@users.noreply.github.com>
2025-11-15 18:45:44 +0300

    add dekstop icons

--- src/components/Desktop/Desktop.tsx
+++ src/components/Desktop/Desktop.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import styles from './Desktop.module.css';
 
 interface DesktopIcon {
@@ -8,22 +9,48 @@ interface DesktopIcon {
 
 const desktopIcons: DesktopIcon[] = [
   { id: 'resume', name: 'Resume', icon: '/assets/PDF.ico' },
+  {
+    id: 'aboutme',
+    name: 'About Me',
+    icon: '/assets/Information.png',
+  },
+  {
+    id: 'contact',
+    name: 'Contact Me',
+    icon: '/assets/mail.png',
+  },
 ];
 
 export const Desktop = () => {
+  const [icons] = useState(desktopIcons);
+  const [selectedIcon, setSelectedIcon] = useState<string | null>(null);
+
   return (
     <div className={styles.desktop}>
-      <div className={styles['icons-container']}>
-        {desktopIcons.map((icon) => (
-          <div key={icon.id} className={styles.icon}>
-            <img
-              src={icon.icon}
-              alt={icon.name}
-              className={styles['icon-image']}
-            />
-            <span className={styles['icon-name']}>{icon.name}</span>
-          </div>
-        ))}
+      <div className={styles['without-taskbar']}>
+        <div className={styles['icons-container']}>
+          {icons.map((icon) => (
+            <div
+              key={icon.id}
+              className={styles.icon}
+              onClick={() => setSelectedIcon(icon.id)}
+            >
+              <img
+                src={icon.icon}
+                alt={icon.name}
+                className={styles['icon-image']}
+                draggable={false}
+              />
+              <span
+                className={`${styles['icon-name']} ${
+                  selectedIcon === icon.id ? styles.selected : ''
+                }`}
+              >
+                {icon.name}
+              </span>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
