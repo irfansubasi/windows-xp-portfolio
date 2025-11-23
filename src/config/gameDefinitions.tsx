9cf9bd57fe672f2a7927053a06a05c6900ba8ffa
irfansubasi <136987051+irfansubasi@users.noreply.github.com>
2025-11-23 23:28:55 +0300

    add gameDefinitions

---
+++ src/config/gameDefinitions.tsx
@@ -0,0 +1,66 @@
+import type { WindowDefinition } from './windowDefinitions';
+
+export const gameDefinitions: WindowDefinition[] = [
+  {
+    id: 'dxBall',
+    name: 'DX Ball',
+    icon: '/assets/games/DxBall/dxball.webp',
+    windowContent: null,
+    windowConfig: {
+      size: { width: 800, height: 600 },
+      hideAddressBar: true,
+      hideToolbar: true,
+      hideMenubar: true,
+    },
+  },
+  {
+    id: 'doom',
+    name: 'Doom',
+    icon: '/assets/games/Doom/doom.ico',
+    windowContent: null,
+    windowConfig: {
+      size: { width: 800, height: 600 },
+      hideAddressBar: true,
+      hideToolbar: true,
+      hideMenubar: true,
+    },
+  },
+  {
+    id: 'quake',
+    name: 'Quake',
+    icon: '/assets/games/Quake/quake.png',
+    windowContent: null,
+    windowConfig: {
+      size: { width: 800, height: 600 },
+      hideAddressBar: true,
+      hideToolbar: true,
+      hideMenubar: true,
+    },
+  },
+  {
+    id: 'pinball',
+    name: 'Pinball',
+    icon: '/assets/games/Pinball/pinball.webp',
+    windowContent: null,
+    windowConfig: {
+      size: { width: 800, height: 600 },
+      hideAddressBar: true,
+      hideToolbar: true,
+      hideMenubar: true,
+    },
+  },
+];
+
+export interface GameInfo {
+  id: string;
+  name: string;
+  icon: string;
+}
+
+export const getGameList = (): GameInfo[] => {
+  return gameDefinitions.map((game) => ({
+    id: game.id,
+    name: game.name,
+    icon: game.icon,
+  }));
+};
