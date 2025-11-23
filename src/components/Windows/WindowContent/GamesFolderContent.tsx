32db80472ea191cfd4a5bea312be59ba4e49ea16
irfansubasi <136987051+irfansubasi@users.noreply.github.com>
2025-11-23 17:37:52 +0300

    add games icons

--- src/components/Windows/WindowContent/GamesFolderContent.tsx
+++ src/components/Windows/WindowContent/GamesFolderContent.tsx
@@ -1,5 +1,54 @@
-import styles from './WindowContent.module.css';
+import styles from './GamesFolderContent.module.css';
+
+interface Game {
+  id: string;
+  name: string;
+  icon: string;
+  onClick: () => void;
+}
+
+const games: Game[] = [
+  {
+    id: 'dxBall',
+    name: 'DX Ball',
+    icon: '/assets/dxball.webp',
+    onClick: () => {},
+  },
+  {
+    id: 'quake',
+    name: 'Quake III',
+    icon: '/assets/quake.png',
+    onClick: () => {},
+  },
+  {
+    id: 'pinball',
+    name: 'Pinball',
+    icon: '/assets/pinball.webp',
+    onClick: () => {},
+  },
+  {
+    id: 'doom',
+    name: 'Doom',
+    icon: '/assets/doom.ico',
+    onClick: () => {},
+  },
+];
 
 export const GamesFolderContent = () => {
-  return <div className={styles.content}></div>;
+  return (
+    <div className={styles.content}>
+      <div className={styles.gameList}>
+        {games.map((game) => (
+          <div className={styles.gameIcon} key={game.id}>
+            <img
+              src={game.icon}
+              alt={game.name}
+              className={styles.gameIconImage}
+            />
+            <span className={styles.gameIconName}>{game.name}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
