230155fb41bb20242bd1fb3d150d65c24509c4a4
irfansubasi <136987051+irfansubasi@users.noreply.github.com>
2025-12-02 18:00:03 +0300

    implement volume context to doom

--- src/components/Windows/WindowContent/Games/DoomGameContent.tsx
+++ src/components/Windows/WindowContent/Games/DoomGameContent.tsx
@@ -1,10 +1,21 @@
 import { DosGameContent } from './DosGameContent';
 import styles from './GameContent.module.css';
+import { useEffect } from 'react';
+import { useVolume } from '../../../../context/VolumeContext';
 
 export default function DoomGameContent() {
+  const { registerApp, unregisterApp } = useVolume();
+
+  useEffect(() => {
+    registerApp('doom', 1);
+    return () => {
+      unregisterApp('doom');
+    };
+  }, [registerApp, unregisterApp]);
+
   return (
     <div className={styles.gameContainer}>
-      <DosGameContent gameUrl="/assets/games/Doom/doom.jsdos" />
+      <DosGameContent gameUrl="/assets/games/Doom/doom.jsdos" appId="doom" />
     </div>
   );
 }
