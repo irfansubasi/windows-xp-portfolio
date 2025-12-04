7b60fb37b21c0ac5c0b794655da1ae6527804031
irfansubasi <136987051+irfansubasi@users.noreply.github.com>
2025-12-04 17:59:13 +0300

    add startmenu layout

--- src/components/StartMenu/StartMenu.tsx
+++ src/components/StartMenu/StartMenu.tsx
@@ -1,3 +1,15 @@
+import styles from './StartMenu.module.css';
+
 export default function StartMenu() {
-  return;
+  return (
+    <div className={styles.startMenu}>
+      <div className={styles.topBar}></div>
+      <div className={styles.orangeLine}></div>
+      <div className={styles.content}>
+        <div className={styles.leftPanel}></div>
+        <div className={styles.rightPanel}></div>
+      </div>
+      <div className={styles.bottomBar}></div>
+    </div>
+  );
 }
